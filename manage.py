782fe67d5612020efb8380ce1eb7f732012e0dc6
Haifeng Wang <haifeng@udel.edu>
2017-11-28 19:38:38 -0500

    new resume added

--- manage.py
+++ manage.py
@@ -1,7 +1,26 @@
 from flask_script import Manager
-from resume import app
+from resume import app, db, Professor, Course
 
 manager = Manager(app)
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    professor1= Professor(name='Julianna Butler', department='Economics')
+    professor2= Professor(name='Jiannan Wang', department='Accounting')
+    course1= Course(courseNumber="MISY350", title="Business Application Development II ", description="Covers concepts related to client side development, including cascading style sheets and JavaScript.", professor=professor2)
+    course2= Course(courseNumber="MISY330", title="Database Design and Implementation ", description="Covers the design and implementation of enterprise databases in the business environment. A networked setting and its effect on database management will be emphasized.")
+    course3= Course(courseNumber="ECON360", title="Government Regulation of Business  ", description="Normative, economic and legal foundations of government intervention in business, particularly antitrust and regulation; and the process and the effects of government policies.", professor=professor1)
+    course4= Course(courseNumber="ECON103", title="Introduction to Macroeconomics ", description="Analyzes the determinants of unemployment, inflation, national income and policy issues relating to how the government alters unemployment and inflation through government spending, taxes and the money supply.")
+    db.session.add(professor1)
+    db.session.add(professor2)
+    db.session.add(course1)
+    db.session.add(course2)
+    db.session.add(course3)
+    db.session.add(course4)
+    db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
--- resume.py
+++ resume.py
@@ -1,23 +1,120 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, session, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess secure key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+class Professor(db.Model):
+    __tablename__ = 'professors'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    department = db.Column(db.Text)
+    courses = db.relationship('Course', backref='professor', cascade="delete")
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    courseNumber= db.Column(db.Text)
+    title = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    course_id = db.Column(db.Integer, db.ForeignKey('professors.id'))
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/courses')
-def get_all_courses():
-    courses =[
-    'MISY160',
-    'MISY350',
-    'ECON360'
-    ]
-    return render_template('courses-all.html', courses = courses)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/professors')
+def show_all_professors():
+    professors = Professor.query.all()
+    return render_template('professor-all.html', professors=professors)
+
+@app.route('/professor/add', methods=['GET', 'POST'])
+def add_professors():
+    if request.method == 'GET':
+        return render_template('professor-add.html')
+    if request.method == 'POST':
+        # get data from the form
+        name = request.form['name']
+        department = request.form['department']
+
+        # insert the data into the database
+        professor = Professor(name=name, department=department)
+        db.session.add(professor)
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+
+@app.route('/professor/edit/<int:id>', methods=['GET', 'POST'])
+def edit_professor(id):
+    professor = Professor.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        return render_template('professor-edit.html', professor=professor)
+    if request.method == 'POST':
+        # update data based on the form data
+        professor.name = request.form['name']
+        professor.department = request.form['department']
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_professors'))
+
+@app.route('/courses')
+def show_all_courses():
+    courses = Course.query.all()
+    return render_template('course-all.html', courses=courses)
+
+
+@app.route('/course/add', methods=['GET', 'POST'])
+def add_courses():
+    if request.method == 'GET':
+        professors = Professor.query.all()
+        return render_template('course-add.html', professors=professors)
+    if request.method == 'POST':
+        # get data from the form
+        courseNumber = request.form['courseNumber']
+        title = request.form['title']
+        description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course = Course(courseNumber=courseNumber, title=title, description=description, professor=professor)
+
+        # insert the data into the database
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
+
+@app.route('/course/edit/<int:id>', methods=['GET', 'POST'])
+def edit_song(id):
+    course = Course.query.filter_by(id=id).first()
+    professors = Professor.query.all()
+    if request.method == 'GET':
+        return render_template('course-edit.html', course=course, professors=professors)
+    if request.method == 'POST':
+        # update data based on the form data
+        course.courseNumber = request.form['courseNumber']
+        course.title = request.form['title']
+        course.description = request.form['description']
+        professor_name = request.form['professor']
+        professor = Professor.query.filter_by(name=professor_name).first()
+        course.professor = professor
+        # update the database
+        db.session.commit()
+        return redirect(url_for('show_all_courses'))
+
 
 
 if __name__ == '__main__':
